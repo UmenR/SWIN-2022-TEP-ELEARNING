ffa23b5650df837d33c08ecf1f5efd28048bcbeb
UmenR <umendrarajapakshe@gmail.com>
2022-10-14 05:47:29 +1100

    feat: add react router v6

---
+++ src/features/home/Home.js
@@ -0,0 +1,11 @@
+
+
+// Home page.
+function HomePage(){
+    return(
+        <h1>Home page</h1>
+    )
+}
+
+
+export default HomePage
---
+++ src/features/quizes/QuizList.js
@@ -0,0 +1,12 @@
+
+// Protected QuizList page
+
+// QuizList page.
+function QuizList(){
+    return(
+        <h1>Quiz List Page</h1>
+    )
+}
+
+
+export default QuizList
