36766b2a9b50a6048a88cf5639af0257bc29625e
dincy <dincy.789@gmail.com>
2022-10-26 03:45:34 +1100

    Adding Layout and CSS

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css';
 
 import App from "./App";
-import { store } from "./store/store";
+import {store} from "./store/store";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById("root")
 );
